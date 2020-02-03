4cdf1d3e675ef589a55bf10f48356fcf6cf10b7f
Rayerdyne <francois.straet@gmail.com>
2020-02-03 16:55:09 +0100

    why not to commit now ?

---
+++ src/fg/fgif.rs
@@ -0,0 +1,200 @@
+extern crate gif;
+
+use gif::{Frame, Encoder, Repeat, SetParameter};
+use std::fs::File;
+use std::io::Error;
+use std::f64::{self, consts::PI};
+use std::borrow::Cow;
+
+#[allow(dead_code)]
+struct MyGif<'a> {
+    encoder: Encoder<&'a mut File>,
+    width: u16,
+    height: u16,
+}
+
+#[allow(dead_code)]
+impl MyGif<'_> {
+    fn new<'a> (file: &'a mut File, w: u16, h: u16, global_palette: &[u8]) -> MyGif<'a> {
+        let mut encoder = Encoder::new(file, w, h, global_palette).unwrap();
+        encoder.set(Repeat::Infinite).unwrap();
+
+        MyGif {
+            width: w,           
+            height: h,
+            encoder: encoder,
+        }
+    }
+
+    fn write_frame(&mut self, t: &[u8]) {
+        let mut frame = Frame::default();
+        frame.width  = self.width;
+        frame.height = self.height;
+        frame.buffer = Cow::Borrowed(&*t);
+        self.encoder.write_frame(&frame).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn draw_line(xi: usize, yi: usize, xf: usize, yf: usize, color: u8,
+             tab: &mut [u8], tabw: usize, tabh: usize) {
+    
+    let xi2 = if xi > tabw {  tabw  } else {  xi  };
+    let xf2 = if xf > tabw {  tabw  } else {  xf  };
+    let yi2 = if yi > tabh {  tabh  } else {  yi  };
+    let yf2 = if yf > tabh {  tabh  } else {  yf  };
+
+    assert!(xi <= tabh && xf <= tabh);
+    assert!(yi <= tabw && yf <= tabw);
+    let (x1, x2, x_inversed) = if xf2 < xi2 { (xf2, xi2, true)   } 
+                               else {       (xi2, xf2, false)  };
+    let (y1, y2, y_inversed) = if yf2 < yi2 { (yf2, yi2, true)   }
+                               else       { (yi2, yf2, false)  };
+    
+    if x1 == x2 {
+        if y1 == y2 {   tab[y1*tabw + x1] = color; 
+                        return }
+        for i in y1..=y2 {   tab[i*tabw + x1] = color;   }
+        return
+    }
+    if y1 == y2 {
+        for i in x1..=x2 {   tab[y1*tabw + i] = color;   }
+        return
+
+    }
+
+    let mut y = y1;
+    let (dx, dy) = ((x2 as i32 - x1 as i32), (y2 as i32 - y1 as i32));
+    let mut e: i32 = -dx;
+    let (ex, ey) = (2*dy, -2*dx);
+
+
+    if !x_inversed {
+        if !y_inversed { // quadrant 4
+            for x in x1..x2 {
+                tab[y*tabw + x] = color;
+
+                e += ex;
+                while e >= 0 {
+                    y += 1;
+                    e += ey;
+                    if e >= 0 {   tab[y*tabw + x] = color;   }
+                }
+            }
+        }
+        else { // quadrant 1 
+            for x in x1..x2 {
+                tab[(yi+yf-y)*tabw + x] = color;
+
+                e += ex;
+                while e >= 0 {
+                    y += 1;
+                    e += ey;
+                    if e >= 0 {   tab[(yi+yf-y)*tabw + x] = color;   }
+                }
+            }
+        }
+    }
+    else { 
+        if !y_inversed { // quadrant 3
+            for x in x1..x2 {
+                tab[y*tabw + xi+xf-x] = color;
+
+                e += ex;
+                while e >= 0 {
+                    y += 1;
+                    e += ey;
+                    if e >= 0 {   tab[y*tabw + xi+xf-x] = color;   }
+                }
+            }
+        }
+        else { // quadrant 2
+            for x in x1..x2 {
+                tab[(yi+yf-y)*tabw + xi+xf-x] = color;
+
+                e += ex;
+                while e >= 0 {
+                    y += 1;
+                    e += ey;
+                    if e >= 0 {   tab[(yi+yf-y)*tabw + xi+xf-x] = color;   }
+                }
+            }
+        }
+    }
+
+}
+
+/* see https://fr.wikipedia.org/wiki/Algorithme_de_trac%C3%A9_de_segment_de_Bresenham */
+
+pub fn draw_fourier_coeff(coeffs: (Vec<[f64; 2]>, Vec<[f64; 2]>), 
+                          filename: &str, w: usize, h: usize, global_palette: &[u8]) -> Result<(), Error> {
+    gotest(20, 20, 200, 200);
+    let (p_coeffs, n_coeffs) = coeffs;
+    let n = p_coeffs.len();
+    assert_eq!(n, n_coeffs.len());
+
+    let mut output = File::create(filename)?;
+    let mut gif = MyGif::new(&mut output, w as u16, h as u16, global_palette);
+    
+    let vect = vec![0; (w*h) as usize];
+    let mut tab_drawing: Box<[u8]> = vect.into_boxed_slice();
+    println!("w: {}, h: {}, {}", w as u16, h as u16, (w*h) as usize);
+    
+    let mut t: f64 = 0.0_f64;
+    let max = 2.0_f64*PI;
+    while t < max {
+        // keep what's already drawed
+        let mut tab_lines = tab_drawing.clone();
+        
+        let mut x1: f64 = (w as f64) / 2.0_f64;
+        let mut y1: f64 = (h as f64) / 2.0_f64;
+        let mut x2: f64 = 0.0_f64;
+        let mut y2: f64 = 0.0_f64;
+
+        let mut k_f64: f64 = 1.0_f64;
+        for k in 0..n {
+            let coeff = (p_coeffs[k], n_coeffs[k]);
+            for (c, neg) in vec![(coeff.0, false),
+                                 (coeff.1, true) ] {
+                let sin1 = if neg {   (k_f64*t).sin()  }
+                           else   {  -(k_f64*t).sin()  };
+                let cos1 = (k_f64*t).cos();
+                //   (a+ib)*(cos + i sin)
+                // = a cos - b sin + i (a sin + b cos)
+                x2 = x1 + (c[0]*cos1 - c[1]*sin1);
+                y2 = y1 + (c[0]*sin1 + c[1]*cos1);
+                draw_line(x1 as usize, y1 as usize, x2 as usize, y2 as usize, 1,
+                    &mut *tab_lines, w, h);
+                x1 = x2;
+                y1 = y2;
+            }
+
+            k_f64 += 1.0_f64;
+        }
+        let (xx, yy) = (x2 as usize, y2 as usize);
+        draw_line(xx, yy, xx, yy, 1, 
+                    &mut *tab_drawing, w, h);
+
+        gif.write_frame(&mut *tab_lines);
+        t += 0.05;
+    };
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub fn gotest(a: usize, b: usize, c: usize, d: usize) {
+    let mut f = File::create("hello.gif").expect("couldn't create file");
+
+    let (w, h) = (300 as usize, 200 as usize);
+    let global_palette = &[0x00, 0x00, 0xFF,
+                           0xFF, 0x00, 0x00];
+    let mut gif = MyGif::new(&mut f, w as u16, h as u16, global_palette);
+
+    let vect = vec![0; w*h];
+    let mut tab: Box<[u8]> = vect.into_boxed_slice();
+    draw_line(a, b, c, d, 1, &mut *tab, w, h);
+    draw_line(100, 100, 150, 0, 1, &mut *tab, w, h);
+    draw_line(100, 100, 50, 200, 1, &mut *tab, w, h);
+    gif.write_frame(&*tab);
+}
--- src/fg/mod.rs
+++ src/fg/mod.rs
@@ -1,22 +1,29 @@
 mod fourier;
-mod gif;
+mod fgif;
 mod spline;
 mod read;
 
+extern crate clap;
+
 use std::fmt;
+use std::num::ParseIntError;
+use clap::{Arg, App};
 
 pub enum FgError {
-    ArgError,
     ReadingError(read::ReadingError),
+    ParseArgError(ParseIntError),
+    IoError(std::io::Error)
 }
 
 impl fmt::Display for FgError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            FgError::ArgError =>
-                { write!(f, "Arguments are not valid !")   }
             FgError::ReadingError(e) => 
                 { write!(f, "Read error: {}", e)           }
+            FgError::ParseArgError(e) =>
+                { write!(f, "Error parsing the provided arguments: {}", e)}
+            FgError::IoError(e) =>
+                { write!(f, "Error creating the output file: {}", e)}
         }
     }
 }
@@ -26,12 +33,80 @@ impl std::convert::From<read::ReadingError> for FgError {
         FgError::ReadingError(e)
     }
 }
+impl std::convert::From<ParseIntError> for FgError {
+    fn from(e: ParseIntError) -> FgError {
+        FgError::ParseArgError(e)
+    }
+}
+impl std::convert::From<std::io::Error> for FgError {
+    fn from(e: std::io::Error) -> FgError {
+        FgError::IoError(e)
+    }
+}
 
 #[allow(dead_code)]
-pub fn read(args: Vec<String>) -> Result<(), FgError> {
-    if args.len() <= 1 {    return Err(FgError::ArgError)   }
+pub fn parse() -> Result<(), FgError> {
     
-    let set = read::read_file(&args[1])?;
+    let matches = App::new("fg")
+            .version("0.1.0")
+            .author("François Straet")
+            .about("Drawings with Fourier series")
+            .arg(Arg::with_name("input")
+                .help("Sets the input file (containing the points of the drawing: `t: (x, y)`")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .help("Sets the name of output file, `output.gif` if not provided"))
+            .arg(Arg::with_name("fcolor")
+                .short("fc")
+                .long("fcolor")
+                .takes_value(true)
+                .help("Sets the foreground color used in the output (hexcode)"))
+            .arg(Arg::with_name("bcolor")
+                .short("bc")
+                .long("bcolor")
+                .takes_value(true)
+                .help("Sets the background color used in the output (hexcode)"))
+            .arg(Arg::with_name("width")
+                .short("w")
+                .long("gifwidth")
+                .takes_value(true)
+                .help("Sets the output's width."))
+            .arg(Arg::with_name("height")
+                .short("h")
+                .long("gifheight")
+                .takes_value(true)
+                .help("Sets the output's width."))
+            .get_matches();
+    
+    let filename = matches.value_of("input").unwrap();
+
+    let fcolor = matches.value_of("fcolor").unwrap_or("0x000000");
+    let fc = color_from_hex(fcolor).unwrap();
+    println!("f color: {} >> {}, {}, {}", fcolor, fc.0, fc.1, fc.2);
+
+    let bcolor = matches.value_of("bcolor").unwrap_or("0xFFFFFF");
+    let bc = color_from_hex(bcolor).unwrap();
+    println!("b color: {} >> {}, {}, {}", bcolor, bc.0, bc.1, bc.2);
+
+    let input = matches.value_of("input").unwrap_or("input.gif");
+    println!("input :{}", input);
+    let output = matches.value_of("output").unwrap_or("output.gif");
+    println!("output: {}", output);
+
+    let sgw = matches.value_of("width").unwrap_or("300");
+    let gw = sgw.parse::<usize>()?;
+    let sgh = matches.value_of("height").unwrap_or("200");
+    let gh = sgh.parse::<usize>()?;
+    
+    fgif::draw_fourier_coeff((vec![[0.0_f64, 50.0_f64]], vec![[0.0_f64, 0.0_f64]]),
+                             output, gw, gh, 
+                             &[bc.0, bc.1, bc.2, fc.0, fc.1, fc.2])?;
+
+    let set = read::read_file(filename)?;
 
     let ss = spline::interpolate_coords(vec![set.get_xx(), set.get_yy()], set.get_tt());
     let sx = ss[0].clone();
@@ -41,6 +116,16 @@ pub fn read(args: Vec<String>) -> Result<(), FgError> {
     Ok(())
 }
 
+#[allow(dead_code)]
 pub fn test_gif(a: usize, b: usize, c: usize, d: usize) {
-    gif::gotest(a, b, c, d);
+    fgif::gotest(a, b, c, d);
+}
+
+fn color_from_hex(s: &str) -> Result <(u8, u8, u8), ParseIntError> {
+    let without_prefix = s.trim_start_matches("0x");
+    let r = u8::from_str_radix(&without_prefix[0..2], 16)?;
+    let g = u8::from_str_radix(&without_prefix[2..4], 16)?;
+    let b = u8::from_str_radix(&without_prefix[4..6], 16)?;
+
+    Ok((r, g, b))
 }
